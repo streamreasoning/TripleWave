2005205af3d35a577fa9edf771539e41aaf59630
Andrea <andrea.mauri87@gmail.com>
2016-06-14 14:29:05 +0200

    fix

--- start.bat
+++ start.bat
@@ -7,15 +7,11 @@ for /F "eol=# delims== tokens=1,*" %%a in (config\config.properties) do (
     rem so some.property becomes test.some.property in batch-land
     rem if NOT "%%a"=="" if NOT "%%b"=="" set config.%%a=%%b
 
-    if "%%a"=="mode"(
-      set use="%%b"
-    )
+    if "%%a"=="mode" set use="%%b"
+    
 )
 
 rem do something useful with your vars
-if NOT %use%=="transform"(
-   java -jar jena-fuseki-server-2.3.1.jar --update --mem \ds &
-   timeout 5
-)
+if NOT %use%=="transform" java -jar jena-fuseki-server-2.3.1.jar --update --mem \ds &, timeout 5
 
 node app.js
